09d314c86dacb12fb9811f108d3b841b9ed906c8
Bikash Mainali <bikash.mainali@ibm.com>
2020-09-16 13:33:54 -0400

    interactive query fixed

--- src/main/java/ibm/gse/eda/inventory/api/InventoryResource.java
+++ src/main/java/ibm/gse/eda/inventory/api/InventoryResource.java
@@ -10,34 +10,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.GenericType;
+
 
 import ibm.gse.eda.inventory.infrastructure.InteractiveQueries;
 import ibm.gse.eda.inventory.infrastructure.InventoryQueryResult;
 import ibm.gse.eda.inventory.infrastructure.PipelineMetadata;
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 
 @ApplicationScoped
 @Path("/inventory")
 public class InventoryResource {
-    
+    private final Client client = ClientBuilder.newBuilder().build();
+
     @Inject
     public InteractiveQueries queries;
 
     @GET
     @Path("/store/{storeID}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<InventoryQueryResult> getStock(@PathParam("storeID") String storeID) {
+    public Response getStock(@PathParam("storeID") String storeID) {
         InventoryQueryResult result = queries.getStoreStock(storeID);
         if (result.getResult().isPresent()) {
             System.out.println("result: " + result.getResult().get().storeName);
-            return Uni.createFrom().item(result);
+            return Response.ok(result.getResult().get()).build();
         } else if (result.getHost().isPresent()) {
-            URI otherUri = getStoreOtherUri(result.getHost().get(), result.getPort().getAsInt(), "/inventory/store", storeID, null);
-            System.out.println("host: " + otherUri.toString());
-            return Uni.createFrom().item(InventoryQueryResult.notFound());
+            System.out.println("data found remotly " + result.getHost());
+            return fetchReeferData(result.getHost().get(), result.getPort().getAsInt(), storeID, new GenericType<Response>() {});
         } else {
-            return  Uni.createFrom().item(InventoryQueryResult.notFound());
+            return Response.status(Status.NOT_FOUND.getStatusCode(), "No data found for container Id " + storeID).build();
         }
     }
 
@@ -48,16 +54,12 @@ public class InventoryResource {
         return Multi.createFrom().items(queries.getStockStoreMetaData().stream());
     }
 
-    private URI getStoreOtherUri(String host, int port, String path, String storeID, String sku) {
-        try {
-            if (storeID == null) {
-                return new URI("http://" + host + ":" + port + path + "/" + sku);
-        
-            } else {
-                return new URI("http://" + host + ":" + port + path + "/" + storeID);
-            }
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
+    private Response fetchReeferData(final String host, final int port, String storeId, GenericType<Response> responseType) {
+        String url = String.format("http://%s:%d//inventory/store/%s", host, port, storeId);
+        System.out.println("Data found on " + url);
+        // System.out.println(url);
+        return client.target(url)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(responseType);
     }
 }
