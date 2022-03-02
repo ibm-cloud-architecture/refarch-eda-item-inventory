7e19b1cc5f377c3981498722fb0eea265b9ee04c
jbcodeforce <jerome.boyer@gmail.com>
2022-03-01 18:19:51 -0800

    add version and mv to 0.0.10

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -2,6 +2,16 @@
 scriptDir=$(dirname $0)
 
 IMAGE_NAME=quay.io/ibmcase/item-aggregator
-./mvnw clean package -Dui.deps -Dui.dev -DskipTests
-docker build -f src/main/docker/Dockerfile.jvm -t ${IMAGE_NAME} .
-docker push ${IMAGE_NAME}
+if [[ $# -eq 1 ]]
+then
+  TAG=$1
+else
+  TAG=latest
+fi
+
+./mvnw clean package  -DskipTests
+docker build -f src/main/docker/Dockerfile.jvm -t  ${IMAGE_NAME}:${TAG} .
+docker tag  ${IMAGE_NAME}:${TAG}   ${IMAGE_NAME}:latest
+docker push ${IMAGE_NAME}:${TAG}
+docker push ${IMAGE_NAME}:latest
+
